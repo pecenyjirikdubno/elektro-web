83160093b255a0809f1719aa62e3646751d27720
Ing. Jiri Peceny <info@jzelektro.cz>
2026-05-02 19:56:54 +0200

    Oprava umisteni a odkazu sluzeb

---
+++ data/sluzby.ts
@@ -0,0 +1,114 @@
+export const services = [
+  {
+    id: "projekce",
+    title: "Projekce elektro",
+    shortText:
+      "Projektová dokumentace elektroinstalací, rozvodů VN/NN a technická řešení.",
+    image: "/images/projekce.png",
+    content: `
+JZ ELEKTRO zajišťuje zpracování projektové dokumentace elektroinstalací pro nové i rekonstruované objekty.
+
+Zaměřujeme se na návrh silnoproudých rozvodů, rozvaděčů, kabelových tras, osvětlení, uzemnění a souvisejících technických řešení.
+
+Součástí práce je technický návrh, konzultace s investorem a příprava řešení s ohledem na bezpečnost, provozní spolehlivost a následnou realizaci.
+`,
+  },
+  {
+    id: "revize-nn",
+    title: "Revize NN",
+    shortText:
+      "Výchozí i pravidelné revize elektrických zařízení nízkého napětí.",
+    image: "/images/revize.png",
+    content: `
+Provádíme výchozí i pravidelné revize elektrických zařízení nízkého napětí.
+
+Revize NN jsou důležité pro bezpečný provoz elektroinstalace, splnění zákonných požadavků a prevenci poruch nebo požáru.
+
+Zajišťujeme kontrolu elektroinstalací, rozvaděčů, strojních zařízení, zásuvkových obvodů, osvětlení a dalších částí elektrického zařízení.
+`,
+  },
+  {
+    id: "revize-vn",
+    title: "Revize VN",
+    shortText:
+      "Revize a kontroly zařízení vysokého napětí včetně měření a diagnostiky.",
+    image: "/images/revize.png",
+    content: `
+Zajišťujeme revize a kontroly zařízení vysokého napětí.
+
+Revize VN zahrnují kontrolu technického stavu zařízení, měření, posouzení bezpečnosti a ověření provozní způsobilosti.
+
+Služba je vhodná pro trafostanice, průmyslové areály, distribuční zařízení a další provozy s technologií VN.
+`,
+  },
+  {
+    id: "elektromontaze",
+    title: "Elektromontáže VN/NN",
+    shortText:
+      "Realizace silnoproudých rozvodů, kabelových tras, rozvaděčů a technologických celků.",
+    image: "/images/montaze.png",
+    content: `
+Provádíme elektromontáže VN/NN pro firmy, průmyslové objekty, veřejné budovy i technickou infrastrukturu.
+
+Realizujeme kabelové trasy, silnoproudé rozvody, rozvaděče, napojení technologií, osvětlení a další elektroinstalační práce.
+
+Cílem je kvalitní a bezpečná realizace s důrazem na dlouhodobou spolehlivost.
+`,
+  },
+  {
+    id: "trafostanice",
+    title: "Trafostanice",
+    shortText:
+      "Dodávka, montáž a servis trafostanic 22/0,4 kV včetně uvedení do provozu.",
+    image: "/images/trafostanice.png",
+    content: `
+Zajišťujeme dodávku, montáž a servis trafostanic 22/0,4 kV.
+
+Realizujeme kioskové i stožárové trafostanice, silnoproudé rozvody, kabelové napojení, technologické vybavení a uvedení zařízení do provozu.
+
+Trafostanice řešíme od technického návrhu až po finální realizaci.
+`,
+  },
+  {
+    id: "hromosvody",
+    title: "Hromosvody",
+    shortText:
+      "Montáž hromosvodů, uzemnění a ochrany proti přepětí.",
+    image: "/images/hromosvod.png",
+    content: `
+Provádíme montáž hromosvodů, uzemnění a souvisejících ochranných prvků.
+
+Správně navržený a provedený hromosvod chrání objekt, technologie i osoby před účinky blesku a přepětí.
+
+Zajišťujeme realizaci pro rodinné domy, průmyslové objekty, veřejné budovy i technologické celky.
+`,
+  },
+  {
+    id: "opravy-naradi",
+    title: "Opravy el. nářadí",
+    shortText:
+      "Servis, diagnostika a opravy elektrického nářadí pro řemeslníky, firmy i provozy.",
+    image: "/images/naradi.png",
+    content: `
+Provádíme servis, diagnostiku a opravy elektrického nářadí.
+
+Služba je vhodná pro řemeslníky, firmy, dílny i provozy, které potřebují rychle vrátit nářadí zpět do provozu.
+
+Zajišťujeme základní diagnostiku závad, opravy, výměnu vadných částí a posouzení ekonomické vhodnosti opravy.
+`,
+  },
+  {
+    id: "opravy-motoru-cerpadel",
+    title: "Opravy elektromotorů a čerpadel",
+    shortText:
+      "Diagnostika, opravy a servis elektromotorů, čerpadel a pohonů.",
+    image: "/images/motor.png",
+    content: `
+Zajišťujeme diagnostiku, opravy a servis elektromotorů, čerpadel a souvisejících pohonů.
+
+Pomáháme řešit poruchy, výpadky provozu, problémy se spouštěním, hlučností, přehříváním nebo mechanickým opotřebením.
+
+Cílem je rychlé posouzení závady a návrh vhodného řešení.
+`,
+  },
+];
